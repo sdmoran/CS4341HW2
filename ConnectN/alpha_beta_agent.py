be240a926311977db5cb6a630b0dadd893352cc4
sdmoran <smoran@wpi.edu>
2019-01-25 00:28:17 -0500

    Implemented alphaBeta method for evaluating successive generations from current board state, as well as get_best_col, which calls alphaBeta to find number of best column to play from current state.

--- ConnectN/alpha_beta_agent.py
+++ ConnectN/alpha_beta_agent.py
@@ -24,17 +24,29 @@ class AlphaBetaAgent(agent.Agent):
     #
     # NOTE: make sure the column is legal, or you'll lose the game.
     def go(self, brd):
+        levels = 4
+
         """Search for the best move (choice of column for the token)"""
         # Selects best move for agent to make based on calculateScore() function.
 
-        candidates = self.get_successors(brd)
+        gen1 = self.get_successors(brd)
+
+        candidates = []
+
+        for c in gen1:
+            localmax = 0
+            boards = self.getRecursiveSuccessors(c[0], levels)
+            for b in boards:
+                score = self.calculateScore(b[0])
+                if score > localmax:
+                    localmax = score
+            candidates.append((score, c))
+
         max = 0
-        move = 0
         for c in candidates:
-            score = self.calculateScore(c[0])
-            if score > max:
-                max = score
-                move = c[1]
+            if c[0] > max:
+                max = c[0]
+                move = c[1][1]
         return move
 
 
@@ -45,20 +57,20 @@ class AlphaBetaAgent(agent.Agent):
         """Heuristic:
             - If the game can be won, do so immediately.
             - Otherwise, look for n - 1s in a row, n - 2s in a row, etc, scoring proportionally"""
+        val = 0
         for col in range(0, brd.w):
             for row in range(0, brd.h):
                 if brd.board[row][col] != 0:
-                    if brd.is_any_line_at(col, row):
-                        return 10000
-
-        # Next, need to check for n-1 in a row.
-
-        return 0
+                    if brd.is_any_line_at(col, row): # check for win
+                        return 100000
+                    if self.is_any_short_line_at(brd, col, row): # check for n-1 in a row
+                        val += 1000
+        return val
 
     # Return the list of the n-th level successors of the given board.
-    # PARAM brd: a board to recursively get successors for
+    # PARAM brd: a Board to recursively get successors for
     # PARAM n: the level to go til
-    # RETURN list of (board.Board)
+    # RETURN tuple of [list of Board.board]
     def getRecursiveSuccessors(self, brd, n):
         children = []
         #print("LEVEL: " + str(n))
@@ -70,9 +82,47 @@ class AlphaBetaAgent(agent.Agent):
                 for c in self.getRecursiveSuccessors(b[0], n - 1):
                     children.append(c)
         return children
-                #print(b[0].board)
-                #print("\n")
 
+
+    def get_best_col(self, brd, n):
+        board_tuples = self.get_successors(brd)
+        maxcol = 0
+        maxval = 0
+        for t in board_tuples:
+            if self.alphaBeta(t[0], n, float('-inf'), float('inf'), 1) > maxval:
+                maxcol = t[1]
+        return maxcol
+
+
+    def alphaBeta(self, brd, n, alpha, beta, player):
+        # todo find more elegant solution fo checking if we won
+        currentScore = self.calculateScore(brd)
+        if n == 0 or currentScore >= 10000:
+            return currentScore
+        # We don't want to return the actual value, we want to return
+        # the column number at the top level that results in highest value
+        # So if it's the top level....
+        # - We get successors of initial board state
+        # - Then run alphaBeta on THOSE
+        # - Get max
+        # - Return index of max
+        # - Probably a job for another method!
+        if player == 1:
+            maxval = float('-inf')
+            for b in self.get_successors(brd):
+                maxval = max(maxval, self.alphaBeta(b[0], n - 1, alpha, beta, 2))
+                alpha = max(alpha, maxval)
+                if alpha > beta:
+                    break
+            return maxval
+        else:
+            minval = float('inf')
+            for b in self.get_successors(brd):
+                minval = min(minval, self.alphaBeta(b[0], n - 1, alpha, beta, 1))
+                beta = min(beta, minval)
+                if alpha > beta:
+                    break
+            return minval
 
     # Get the successors of the given board.
     #
@@ -98,3 +148,40 @@ class AlphaBetaAgent(agent.Agent):
             # Add board to list of successors
             succ.append((nb,col))
         return succ
+
+    def is_short_line_at(self, brd, x, y, dx, dy):
+        """Return True if a line of identical tokens exists starting at (x,y) in direction (dx,dy)"""
+        # Avoid out-of-bounds errors
+        if ((x + (brd.n - 1) * dx >= brd.w) or
+            (y + (brd.n - 1) * dy < 0) or (y + (brd.n - 1) * dy >= brd.h)):
+            return False
+        # Get token at (x,y)
+        t = brd.board[y][x]
+        # Go through elements
+        for i in range(1, brd.n - 1):
+            if brd.board[y + i * dy][x + i * dx] != t:
+                return False
+        return True
+
+    # Check if a line of identical tokens exists starting at (x,y) in any direction
+    #
+    # PARAM [int] x:  the x coordinate of the starting cell
+    # PARAM [int] y:  the y coordinate of the starting cell
+    # RETURN [Bool]: True if n tokens of the same type have been found, False otherwise
+    def is_any_short_line_at(self, brd, x, y):
+        """Return True if a line of identical tokens exists starting at (x,y) in any direction"""
+        return (self.is_short_line_at(brd, x, y, 1, 0) or  # Horizontal
+                self.is_short_line_at(brd, x, y, 0, 1) or  # Vertical
+                self.is_short_line_at(brd, x, y, 1, 1) or  # Diagonal up
+                self.is_short_line_at(brd, x, y, 1, -1)) # Diagonal down
+
+    # Calculate the game outcome.
+    #
+    # RETURN [int]: 1 for Player 1, 2 for Player 2, and 0 for no winner
+    def get_outcome(self):
+        """Returns the winner of the game: 1 for Player 1, 2 for Player 2, and 0 for no winner"""
+        for x in range(self.w):
+            for y in range(self.h):
+                if (self.board[y][x] != 0) and self.is_any_short_line_at(x,y):
+                    return self.board[y][x]
+        return 0
--- ConnectN/tester.py
+++ ConnectN/tester.py
@@ -2,7 +2,21 @@ import board
 import alpha_beta_agent as aba
 
 brd = [
-    [1, 1, 0, 1, 1],
+    [1, 1, 1, 0, 1],
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0]
+]
+
+empty = [
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0]
+]
+
+wbrd = [
+    [2, 1, 1, 1, 1],
     [0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0]
@@ -10,11 +24,20 @@ brd = [
 
 b1 = board.Board(brd, 5, 4, 4)
 
+win = board.Board(wbrd, 5, 4, 4)
+
+blank = board.Board(empty, 5, 4, 4)
 
 
 agent = aba.AlphaBetaAgent("Jeffery", 5)
 
-print(agent.calculateScore(b1))
+# If game is already won, then method to calculate which column to move next will return 0, because
+# any move results in "win" state, meaning children[0] is a win so column 0 is the move. Yay!
+# print("Win score: ")
+# print(agent.calculateScore(win))
+# print("Column: ")
+# print(agent.go(brd))
+
 
 """for b in boards:
     tier2 = agent.get_successors(b[0])
@@ -26,17 +49,17 @@ print(agent.calculateScore(b1))
 
 # It's only looking at the 1st successor of current board state, where a "1" piece is placed in col 0, so it won't ever
 # detect a win in column 0. Remember this later, silly.
-for b in agent.getRecursiveSuccessors(b1, 1):
+for b in agent.getRecursiveSuccessors(b1, 2):
     # if agent.calculateScore(b[0]) > 1:
         for n in range(0, 4):
             print(b[0].board[n])
         print("==== ====")
-
-print("REGULAR GETSUCCESSORS: \n\n\n")
-
-for b in agent.get_successors(b1):
-    print(b[0].board)
 # yay, they're the same.
 
 print(agent.go(b1))
+
+print(agent.get_best_col(b1, 4))
+
+print(agent.alphaBeta(win, 10, float('-inf'), float('inf'), 1))
+
 
